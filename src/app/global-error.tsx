a5ffdbb60124c5fdbdc35e403240c09c3993e9d6
Ihor Berezhnyi <iberezhnyi@gmail.com>
2024-09-29 13:27:34 +0300

    fix error

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -4,13 +4,12 @@ import React from 'react'
 
 export interface GlobalErrorProps {}
 
-export default function GlobalError({ error }: GlobalErrorProps) {
+export default function GlobalError({}: GlobalErrorProps) {
   return (
     <html>
       <body>
         <div>
           <p>Something globally went wrong</p>
-          <p>{error()}</p>
         </div>
       </body>
     </html>
