4ced2178c7f84be6ac26dd81fff63ae9530b10b4
Ihor Berezhnyi <iberezhnyi@gmail.com>
2024-10-01 16:55:09 +0300

    fix(not-found, global-error): fix errors pages

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -3,13 +3,18 @@
 import { FC } from 'react'
 import Link from 'next/link'
 import Header from '@/components/common/Header'
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
 
 export interface IGlobalErrorProps {
-  message: string
-  onRetry?: () => void
+  error: Error
 }
 
-const GlobalError: FC<IGlobalErrorProps> = ({ message, onRetry }) => {
+const GlobalError: FC<IGlobalErrorProps> = ({ error }) => {
+  const handleRetry = () => {
+    window.location.reload()
+  }
+
   return (
     <html>
       <body>
@@ -17,21 +22,19 @@ const GlobalError: FC<IGlobalErrorProps> = ({ message, onRetry }) => {
           <Header />
 
           <div className="flex flex-col items-center justify-center p-4 rounded-md">
+            <Image src="/assets/errors/222.png" alt="" className="mb-4"></Image>
             <h1 className="text-2xl font-bold mb-4">An Error Occurred</h1>
-            <h2 className="text-xl font-bold mb-2">Error</h2>
-            <p className="mb-4 text-center">{message}</p>
+            <h2 className="text-xl font-bold mb-2">{error.message}</h2>
+            <p className="mb-4">Please</p>
             <div>
-              <button
-                onClick={onRetry}
-                className="bg-accent text-white px-4 py-2 rounded-md hover:bg-accent-hover transition"
-              >
+              <Button onClick={handleRetry} className="mb-4">
                 Retry
-              </button>
+              </Button>
             </div>
-            <p>Or</p>
+            <p className="mb-4">Or back to</p>
             <Link
               href="/"
-              className="bg-accent text-white px-4 py-2 rounded-md hover:bg-accent-hover transition"
+              className="inline-flex items-center justify-center whitespace-nowrap rounded-full text-base font-semibold ring-offset-white transition-colors uppercase gap-2 border border-accent bg-transparent text-accent hover:bg-accent hover:text-primary h-[44px] px-6 tracking-[2px]"
             >
               Home
             </Link>
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,13 +3,14 @@
 import { FC } from 'react'
 import Link from 'next/link'
 import Header from '@/components/common/Header'
+// import { Button } from '@/components/ui/button'
+import Image from 'next/image'
 
-export interface INotFoundProps {
-  message: string
-  onRetry?: () => void
-}
+const NotFound: FC = () => {
+  // const handleRetry = () => {
+  //   window.location.reload()
+  // }
 
-const NotFound: FC<INotFoundProps> = ({ message, onRetry }) => {
   return (
     <html>
       <body>
@@ -17,21 +18,27 @@ const NotFound: FC<INotFoundProps> = ({ message, onRetry }) => {
           <Header />
 
           <div className="flex flex-col items-center justify-center p-4 rounded-md">
+            <Image src="/assets/errors/222.png" alt="" className="mb-4"></Image>
+            <h2 className="text-4xl font-bold mb-2">404</h2>
             <h1 className="text-2xl font-bold mb-4">Page Not Found</h1>
-            <h2 className="text-xl font-bold mb-2">404</h2>
-            <p className="mb-4 text-center">{message}</p>
-            <div>
-              <button
-                onClick={onRetry}
-                className="bg-accent text-white px-4 py-2 rounded-md hover:bg-accent-hover transition"
-              >
+            {/* <h2 className="text-xl font-bold mb-2">{error}</h2> */}
+            <p>The page you are looking for doesn&apos;t exist.</p>
+            <p></p>
+            <p className="mb-4">Please</p>
+            {/* <div>
+              <Button onClick={handleRetry} className="mb-4">
                 Retry
-              </button>
-            </div>
-            <p>Or</p>
+              </Button>
+            </div> */}
+            {/* <p className="mb-4">
+              Or back to the home page to choose a new direction.
+            </p> */}
+            <p className="mb-4">
+              Go back to the home page to choose a new direction.
+            </p>
             <Link
               href="/"
-              className="bg-accent text-white px-4 py-2 rounded-md hover:bg-accent-hover transition"
+              className="inline-flex items-center justify-center whitespace-nowrap rounded-full text-base font-semibold ring-offset-white transition-colors uppercase gap-2 border border-accent bg-transparent text-accent hover:bg-accent hover:text-primary h-[44px] px-6 tracking-[2px]"
             >
               Home
             </Link>
@@ -39,6 +46,34 @@ const NotFound: FC<INotFoundProps> = ({ message, onRetry }) => {
         </main>
       </body>
     </html>
+    // <html>
+    //   <body>
+    //     <main className="h-[100vh]">
+    //       <Header />
+
+    //       <div className="flex flex-col items-center justify-center p-4 rounded-md">
+    //         <h1 className="text-2xl font-bold mb-4">Page Not Found</h1>
+    //         <h2 className="text-xl font-bold mb-2">404</h2>
+    //         <p className="mb-4 text-center">{message}</p>
+    //         <div>
+    //           <button
+    //             onClick={onRetry}
+    //             className="bg-accent text-white px-4 py-2 rounded-md hover:bg-accent-hover transition"
+    //           >
+    //             Retry
+    //           </button>
+    //         </div>
+    //         <p>Or</p>
+    //         <Link
+    //           href="/"
+    //           className="bg-accent text-white px-4 py-2 rounded-md hover:bg-accent-hover transition"
+    //         >
+    //           Home
+    //         </Link>
+    //       </div>
+    //     </main>
+    //   </body>
+    // </html>
   )
 }
 
