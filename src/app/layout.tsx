a16f00a40d2497514ea89d7cae1b11ab42f5ffd2
Ihor Berezhnyi <iberezhnyi@gmail.com>
2024-10-10 14:48:28 +0300

    feature(favicon, projects data): add favicon, add new projects to the projectsData

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,23 @@ interface RootLayoutProps {
 const RootLayout: FC<Readonly<RootLayoutProps>> = ({ children }) => {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon/favicon-48x48.png"
+          sizes="48x48"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+      </head>
+
       <body className={jetBrainsMono.variable}>
         <ClientLayout>{children}</ClientLayout>
 
--- src/data/projects/projectsData.ts
+++ src/data/projects/projectsData.ts
@@ -4,18 +4,17 @@ export const allProjects: IProject[] = [
   {
     num: '01',
     category: 'fullstack',
-    title: 'Water Tracker',
-    team: true,
+    title: 'Planner APP',
+    team: false,
     description:
-      'Is an app that allows users to track their daily water consumption',
+      'An APP, designed to help users manage their daily tasks. The app includes user authentication, data persistence and user-friendly interface',
     stack: [
+      { name: 'TypeScript' },
       { name: 'React JS' },
       { name: 'MongoDB' },
       { name: 'JWT' },
       { name: 'Emotion Styled' },
-      { name: 'Node JS' },
-      { name: 'Express JS' },
-      { name: 'Swagger' },
+      { name: 'Nest JS' },
 
       // { name: 'OavaScrOt HTMO' },
       // { name: 'OavaScrOt HTMO' },
@@ -35,6 +34,32 @@ export const allProjects: IProject[] = [
       // { name: 'OavaScrOt HTMO' },
       // { name: 'OavaScrOt HTMO' },
     ],
+
+    image: '/assets/projects/planner-project.png',
+    live: 'https://iberezhnyi.github.io/planner/',
+    github: {
+      frontend: 'https://github.com/iberezhnyi/planner',
+      backend: 'https://github.com/iberezhnyi/planner-backend',
+    },
+  },
+
+  {
+    num: '02',
+    category: 'fullstack',
+    title: 'Water Tracker APP',
+    team: true,
+    description:
+      'is an App that allows users to track their daily water consumption',
+    stack: [
+      { name: 'React JS' },
+      { name: 'MongoDB' },
+      { name: 'JWT' },
+      { name: 'Emotion Styled' },
+      { name: 'Node JS' },
+      { name: 'Express JS' },
+      { name: 'Swagger' },
+    ],
+
     image: '/assets/projects/water-tracker-project.jpg',
     live: 'https://anzhela-ostrovska1.github.io/goit-group2-react-nodejs/',
     github: {
@@ -46,8 +71,32 @@ export const allProjects: IProject[] = [
   },
 
   {
-    num: '02',
-    category: 'frontend',
+    num: '03',
+    category: 'fullstack',
+    title: 'Phone book APP',
+    team: false,
+    description:
+      'An APP for managing personal contacts. Users can add, and delete contacts, with secure authentication and a simple, intuitive interface for easy contact management.',
+    stack: [
+      { name: 'React JS' },
+      { name: 'MongoDB' },
+      { name: 'JWT' },
+      { name: 'Emotion Styled' },
+      { name: 'Node JS' },
+      { name: 'Express JS' },
+    ],
+
+    image: '/assets/projects/phonebook-project.png',
+    live: 'https://iberezhnyi.github.io/phonebook/',
+    github: {
+      frontend: 'https://github.com/iberezhnyi/phonebook',
+      backend: 'https://github.com/iberezhnyi/phonebook-backend',
+    },
+  },
+
+  {
+    num: '04',
+    category: 'JavaScript',
     title: 'Filmoteka',
     team: true,
     description:
@@ -65,8 +114,27 @@ export const allProjects: IProject[] = [
   },
 
   {
-    num: '03',
-    category: 'frontend',
+    num: '05',
+    category: 'JavaScript',
+    title: 'ImageFinder',
+    team: false,
+    description:
+      'A website with a responsive layout. Image search service utilizing functional JavaScript for seamless functionality and user experience.',
+    stack: [
+      { name: 'JavaScript' },
+      { name: 'Html 5' },
+      { name: 'Css 3' },
+      { name: 'SASS' },
+      { name: 'BEM' },
+    ],
+    image: '/assets/projects/image-finder.png',
+    live: 'https://iberezhnyi.github.io/image-finder/',
+    github: { frontend: 'https://github.com/iberezhnyi/image-finder' },
+  },
+
+  {
+    num: '06',
+    category: 'Markup',
     title: 'Icecream',
     team: true,
     description:
@@ -80,5 +148,23 @@ export const allProjects: IProject[] = [
     image: '/assets/projects/icecream-project.jpg',
     live: 'https://i-havr.github.io/icecream-team-project/',
     github: { frontend: 'https://github.com/i-havr/icecream-team-project' },
+  },
+
+  {
+    num: '07',
+    category: 'Markup',
+    title: 'WebStudio',
+    team: false,
+    description:
+      'Responsive design, transform effects, modal windows. With the help of CSS, SASS mixins and placeholders. Everything was implemented using CSS. JS was not used.',
+    stack: [
+      { name: 'Html 5' },
+      { name: 'Css 3' },
+      { name: 'SASS' },
+      { name: 'BEM' },
+    ],
+    image: '/assets/projects/web-studio-project.jpg',
+    live: 'https://iberezhnyi.github.io/web-studio-markup/',
+    github: { frontend: 'https://github.com/iberezhnyi/web-studio-markup' },
   },
 ]
